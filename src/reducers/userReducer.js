95d162ff58e23bc1cffa91d086673462e8852aa4
ccyberbear <duycazami92012001@gmail.com>
2022-03-15 22:27:22 +0700

    ready

---
+++ src/reducers/userReducer.js
@@ -0,0 +1,13 @@
+const initialValue = {};
+
+const userReducer = (state = initialValue, action) => {
+  switch (action.type) {
+    case 1:
+      return;
+    case 2:
+      return;
+    default:
+      return state;
+  }
+};
+export default userReducer;
---
+++ src/store.js
@@ -0,0 +1,6 @@
+import { applyMiddleware, compose, createStore } from "redux";
+import rootReducer from "./reducers/rootReducer";
+import thunk from "redux-thunk";
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
+export default store;
