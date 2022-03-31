9382e7f50e3d63f8114e1f17bc9f2fa25083cc11
ccyberbear <duycazami92012001@gmail.com>
2022-03-31 09:17:59 +0700

    cacthError

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -4,25 +4,33 @@ import ProductStyle1 from "../ProductStyle1/ProductStyle1";
 import { useSelector } from "react-redux";
 import Navigation from "../Navigation/Navigation";
 import { Col, Row } from "antd";
+import { StopOutlined } from "@ant-design/icons";
 const Search = () => {
   const searchitem = useSelector((state) => state.productReducer.search);
   return (
     <div className="Search">
       <Navigation />
       <div className="container">
-        <Row style={{ width: "100%" }}>
-          {searchitem.map((pro) => (
-            <Col span={6}>
-              <ProductStyle1
-                id={pro._id}
-                name={pro.name}
-                img={pro.img}
-                retailPrice={pro.retailPrice}
-                type={pro.subCategory}
-              />
-            </Col>
-          ))}
-        </Row>
+        {searchitem.length ? (
+          <Row style={{ width: "100%" }}>
+            {searchitem.map((pro) => (
+              <Col span={6}>
+                <ProductStyle1
+                  id={pro._id}
+                  name={pro.name}
+                  img={pro.img}
+                  retailPrice={pro.retailPrice}
+                  type={pro.subCategory}
+                />
+              </Col>
+            ))}
+          </Row>
+        ) : (
+          <div className="one">
+            <StopOutlined />
+            <h1>404 Not found</h1>
+          </div>
+        )}
       </div>
     </div>
   );
