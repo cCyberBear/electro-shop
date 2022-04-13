9bef8bdd035dbb73153f194875272711009263c7
ccyberbear <duycazami92012001@gmail.com>
2022-04-13 09:42:58 +0700

    test

--- src/store.js
+++ src/store.js
@@ -1,13 +1,14 @@
 import { applyMiddleware, compose, createStore } from "redux";
 import rootReducer from "./reducers/rootReducer";
 import thunk from "redux-thunk";
-import { persistStore, persistReducer } from "redux-persist";
+import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const persistConfig = {
   key: "root",
   storage,
+  whitelist: ["shop"],
 };
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 const store = createStore(
